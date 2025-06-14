dd1869bdabf93195ddeeb472ef6a5c08bc225bbb
CrazyBhai8 <vyommodi18@gmail.com>
2025-06-14 10:55:42 +0530

    change defult theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { Moon, Sun } from 'lucide-react'
 import { useState } from 'react'
 
 const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false)
+    const [isDarkMode, setIsDarkMode] = useState(ture)
 
     useEffect(() => {
         const storedTheme = localStorage.getItem('theme')
