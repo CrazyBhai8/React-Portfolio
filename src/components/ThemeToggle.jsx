561903a3b67e55908b2e84f1cb2cb3d41897c1fd
CrazyBhai8 <vyommodi18@gmail.com>
2025-06-14 10:59:20 +0530

    make a mistake so once agin change defult theme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { Moon, Sun } from 'lucide-react'
 import { useState } from 'react'
 
 const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(ture)
+    const [isDarkMode, setIsDarkMode] = useState(true)
 
     useEffect(() => {
         const storedTheme = localStorage.getItem('theme')
