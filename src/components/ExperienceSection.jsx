678e21963b3d6c2d9a462d825ddf598bc4a988f2
CrazyBhai8 <vyommodi18@gmail.com>
2025-06-16 14:21:09 +0530

    Solve logo err Experience Section

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -2,8 +2,8 @@ import { Description } from '@radix-ui/react-toast'
 import React from 'react'
 
 const experience = [
-    {id:1, company:"Soluner Tech Solutions", position:"Software Intern", duration:"June 2025 - Present", logo:"/company logos/solunertech.svg"},
-    {id:2, company:"The Karma Foundation", position:"Volunteer", duration:"June 2024 - June 2024", description:`- Conducted sessions in rural schools promoting reading habits. <br>- Helped students improve communication and confidence. <br> - Supported educational initiatives driven by college programs` , logo: "/company logos/Karma-Foundation-Logo.webp"},
+    {id:1, company:"Soluner Tech Solutions", position:"Software Intern", duration:"June 2025 - Present", logo:"/company%20logos/solunertech.svg"},
+    {id:2, company:"The Karma Foundation", position:"Volunteer", duration:"June 2024 - June 2024", description:`- Conducted sessions in rural schools promoting reading habits. <br>- Helped students improve communication and confidence. <br> - Supported educational initiatives driven by college programs` , logo: "/company%20logos/Karma-Foundation-Logo.webp"},
 ]
 
 const ExperienceSection = () => {
