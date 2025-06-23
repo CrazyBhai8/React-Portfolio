74e5f2264aa90db85a4f6f72f26b40dcbeedc197
CrazyBhai8 <vyommodi18@gmail.com>
2025-06-23 22:33:53 +0530

    add digiways exprieance

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -2,8 +2,9 @@ import { Description } from '@radix-ui/react-toast'
 import React from 'react'
 
 const experience = [
-    {id:1, company:"Soluner Tech Solutions", position:"Software Intern", duration:"June 2025 - Present", logo:"/company%20logos/solunertech.svg"},
-    {id:2, company:"The Karma Foundation", position:"Volunteer", duration:"June 2024 - June 2024", description:`- Conducted sessions in rural schools promoting reading habits. <br>- Helped students improve communication and confidence. <br> - Supported educational initiatives driven by college programs` , logo: "/company%20logos/Karma-Foundation-Logo.webp"},
+    {id:1, company:"Soluner Tech Solutions", position:"MERN Stack Developer", duration:"Oct 2024 - Present", logo:"/company%20logos/solunertech.svg"},
+    {id:2, company:"Digiways.in", position:"Jr Full Stack Developer", duration:"Mar 2023 - Sep 2024", logo:"/company%20logos/digiways.png", description:"Worked on multiple projects including a web application for a leading e-commerce company"},
+    {id:3, company:"The Karma Foundation", position:"Volunteer", duration:"June 2024 - June 2024", description:`- Conducted sessions in rural schools promoting reading habits. <br>- Helped students improve communication and confidence. <br> - Supported educational initiatives driven by college programs` , logo: "/company%20logos/Karma-Foundation-Logo.webp"},
 ]
 
 const ExperienceSection = () => {
